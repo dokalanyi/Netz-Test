22a13f5f28ff9dd3ee5ee2cc32e33883055417d9
Okalany Daniel <dokalanyi@gmail.com>
2017-07-04 12:51:37 +0300

    Added PIN on current location, PIN on tap

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    let locationManager =  CLLocationManager()
+    let newPin = MKPointAnnotation()
+    
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+
+        locationManager.startUpdatingLocation()
+        
+        let longPress = UITapGestureRecognizer(target: self, action: #selector(ViewController.mapTapped(_:))) // colon needs to pass through info
+        map.addGestureRecognizer(longPress)
+
     }
 
+    func mapTapped(_ recognizer: UIGestureRecognizer) {
+        let touchedAt = recognizer.location(in: self.map)
+        let touchedAtCoordinate : CLLocationCoordinate2D = map.convert(touchedAt, toCoordinateFrom: self.map)
+        
+        let newPin = MKPointAnnotation()
+        newPin.coordinate = touchedAtCoordinate
+        map.addAnnotation(newPin)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        map.removeAnnotation(newPin)
+        
+        let location = locations.last! as CLLocation
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        map.setRegion(region, animated: true)
+        newPin.coordinate = location.coordinate
+        map.addAnnotation(newPin)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
